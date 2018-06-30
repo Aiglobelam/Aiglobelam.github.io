1c2ca116d3b37a7ce794a8e3175f7e9042e32954
jens-held-aftonbladet <jens.held@aftonbladet.se>
2018-07-01 01:28:26 +0200

    Handling missing routes with Switch

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ var Popular = require('./Popular');
 var ReactRouterDom = require('react-router-dom');
 var Router = ReactRouterDom.BrowserRouter;
 var Route = ReactRouterDom.Route;
+var Switch = ReactRouterDom.Switch;
 
 var Nav = require('./Nav');
 var Home = require('./Home');
@@ -13,6 +14,8 @@ var Battle = require('./Battle');
 // UI (required)
 class App extends React.Component {
     
+    defaultRouteRender = () => <p>Route not found seru...</p>;
+
     //--------//
     //-- UI --//
     //--------//
@@ -21,9 +24,18 @@ class App extends React.Component {
             <Router>
                 <div className='container'>
                     <Nav />
-                    <Route exact path='/' component={Home} />
-                    <Route path='/battle' component={Battle} />
-                    <Route path='/popular' component={Popular} />
+                    { /* 
+                        Instead of render all of the routes that are active,
+                        Switch, is going to render one specific Route even if several matches.
+                        So when using Swithc "exact is not needed..." !?!?!?!?
+                        <Route render={defaultRouteRender} />
+                    */ }
+                    <Switch>
+                        <Route exact path='/' component={Home} />
+                        <Route exact path='/battle' component={Battle} />
+                        <Route path='/popular' component={Popular} />
+                        <Route render={this.defaultRouteRender} />
+                    </Switch>
                 </div>
             </Router>
             
